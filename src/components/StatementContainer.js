f43730633ee4724224c27958e24e08bbd9e1c3d9
Victor Vargas <vvargasnunez@gmail.com>
2020-12-30 16:23:12 -0800

    Add unique ids for statement items

--- src/components/StatementContainer.js
+++ src/components/StatementContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import StatementForm from './StatementForm';
 import StatementItem from './StatementItem';
+import {v4 as uuid} from 'uuid';
 
 class Statement extends Component {
 	constructor(props) {
@@ -11,28 +12,33 @@ class Statement extends Component {
 				{
 					statement: 'I am hungry',
 					imgSrc:
-						'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/gettyimages-79050550.jpg?itok=M9SXlxFd'
+						'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/gettyimages-79050550.jpg?itok=M9SXlxFd',
+					id: uuid()
 				},
 				{
 					statement: 'I am sleepy',
-					imgSrc: 'https://images.news18.com/ibnlive/uploads/2020/06/1592996485_untitled-design-93.jpg'
+					imgSrc: 'https://images.news18.com/ibnlive/uploads/2020/06/1592996485_untitled-design-93.jpg',
+					id: uuid()
 				},
 				{
 					statement: 'I am excited',
-					imgSrc: 'https://cdn.pixabay.com/photo/2017/08/16/22/29/excited-2649320_1280.jpg'
+					imgSrc: 'https://cdn.pixabay.com/photo/2017/08/16/22/29/excited-2649320_1280.jpg',
+					id: uuid()
 				},
 				{
 					statement: 'I am stressed',
 					imgSrc:
-						'https://www.personneltoday.com/wp-content/uploads/sites/8/2018/04/stressed-IT-employees.jpg'
+						'https://www.personneltoday.com/wp-content/uploads/sites/8/2018/04/stressed-IT-employees.jpg',
+					id: uuid()
 				}
 			]
 		};
 	}
 
 	addItem = (item) => {
+		let newItem = { ...item, id: uuid() };
 		this.setState((curState) => ({
-			entries: [...curState.entries, item]
+			entries: [...curState.entries, newItem]
 		}));
 	}
 
@@ -50,9 +56,9 @@ class Statement extends Component {
 		return (
 			<div>
 				<StatementForm addItem={this.addItem} /> 
-				{entries.slice(0).reverse().map((item, index) => {
+				{entries.slice(0).reverse().map((item) => {
 						return (<StatementItem
-							key={index}
+							key={item.id}
 							statement={item.statement}
 							imgSrc={item.imgSrc}
 							onClick={this.handleClick}
